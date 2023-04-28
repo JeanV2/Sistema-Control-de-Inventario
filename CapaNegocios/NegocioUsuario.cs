385158971214121171708a3d34ffa229dc6fb147
JeanV2 <126120857+JeanV2@users.noreply.github.com>
2023-04-28 17:06:24 -0600

    cambios

--- CapaNegocios/NegocioUsuario.cs
+++ CapaNegocios/NegocioUsuario.cs
@@ -12,7 +12,7 @@ namespace CapaNegocios
     public class NegocioUsuario
     {
         //prueba
-        //hola
+        //holadddd
          DatosUsuario DatosUsuario = new DatosUsuario();
         public List<TbColaborador> obtenerusuario()
         {
