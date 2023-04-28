4f054523572f6fb0d77bd15552fba81bc240c8c6
JeanV2 <126120857+JeanV2@users.noreply.github.com>
2023-04-28 11:16:23 -0600

    g

---
+++ CapaUtilidades/Interfaces/IGenerica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaUtilidades.Interfaces
+{
+    public interface IGenerica<entidad>
+    {
+        bool Guardar(entidad entidad);
+        bool Actualizar(entidad entidad);
+        bool Eliminar(entidad entidad);
+        List<entidad> Lista(entidad entidad);
+
+    }
+}
