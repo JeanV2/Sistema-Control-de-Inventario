0998990b49cce2c748652dbd56a2cf97ed0388b7
FranRuiz31 <fRANiSA31@>
2023-04-29 11:05:52 -0600

    Falta filtro por producto

---
+++ CapaDatos/DatosProductos.cs
@@ -0,0 +1,99 @@
+﻿using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class DatosProductos
+    {
+
+        /// <summary>
+        /// Guarda Productos en la tabla
+        /// </summary>
+        /// <param name="producto"></param>
+        /// <returns></returns>
+        public bool GuardarProduct(TbProducto producto)
+        {
+            try
+            {
+                using (var C = new inventarioEntities1())
+                {
+                    C.TbProducto.Add(producto);
+                    C.SaveChanges();
+                }
+                return true;
+
+            }
+            catch (Exception exe)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Modifica los productos
+        /// </summary>
+        /// <param name="producto"></param>
+        /// <returns></returns>
+        public bool ModificarProduct(TbProducto producto)
+        {
+            try
+            {
+                using (var C = new inventarioEntities1())
+                {
+                    C.Entry<TbProducto>(producto).State = System.Data.Entity.EntityState.Modified;
+                    C.SaveChanges();
+                }
+                return true;
+
+            }
+            catch (Exception exe)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Elimina los productos
+        /// </summary>
+        /// <param name="producto"></param>
+        /// <returns></returns>
+        public bool EliminarProduct(TbProducto producto)
+        {
+            try
+            {
+                using (var C = new inventarioEntities1())
+                {
+                    C.Entry<TbProducto>(producto).State = System.Data.Entity.EntityState.Deleted;
+                    C.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception exe)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Lista de todos los productos Guardados
+        /// </summary>
+        /// <returns></returns>
+        public List<TbProducto> ListProduct()
+        {
+            try
+            {
+                using (var C = new inventarioEntities1())
+                {
+                    return (from P in C.TbProducto
+                            select P).ToList();
+                }
+
+            }catch (Exception ex)
+            {
+                return null;
+            }
+         
+        }
+    }
+}
--- CapaPresentacion/FrmListaProductos.cs
+++ CapaPresentacion/FrmListaProductos.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaEntidades;
+using CapaNegocios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -31,6 +33,16 @@ namespace CapaPresentacion
         {
             InitializeComponent();
         }
+        //Lista
+        List<TbProducto> lisProductos;
+        //Entidad
+        TbProducto producto = new TbProducto();
+        //
+        NegociosProductos NegProd = new NegociosProductos();
+
+        public delegate void pasarDatos(TbProducto product);
+        public event pasarDatos pasarDatosEvent;
+        //-------------------------------------------------------------------------------------------------------------
 
         private void BtnVolver_Click(object sender, EventArgs e)
         {
@@ -47,7 +59,8 @@ namespace CapaPresentacion
                 AñadirProductos.TxtCodigoProducto.Text = DgvListaProductos.Rows[fila].Cells[0].Value.ToString();
                 AñadirProductos.TxtNombreProducto.Text = DgvListaProductos.Rows[fila].Cells[1].Value.ToString();
                 AñadirProductos.TxtCantidad.Text = DgvListaProductos.Rows[fila].Cells[2].Value.ToString();
-                AñadirProductos.TxtDescripcion.Text = DgvListaProductos.Rows[fila].Cells[3].Value.ToString();
+               // AñadirProductos.TxtCantidad.Text = DgvListaProductos.Rows[fila].Cells[3].Value.ToString(); COSTO
+                AñadirProductos.TxtDescripcion.Text = DgvListaProductos.Rows[fila].Cells[4].Value.ToString();
 
 
                 AñadirProductos.BtnGuardar.Enabled = false;
@@ -59,15 +72,18 @@ namespace CapaPresentacion
                 // PASA LOS DATOS AL FORMULARIO SOLICITUD DE INSUMOS
                 frmSolicitud.TxtCodigoProcd.Text = DgvListaProductos.Rows[fila].Cells[0].Value.ToString();
                 frmSolicitud.TxtNombreProduc.Text = DgvListaProductos.Rows[fila].Cells[2].Value.ToString();
-                
+
                 this.Close();
             }
         }
 
         private void FrmListaProductos_Load(object sender, EventArgs e)
         {
-
+            lisProductos = NegProd.ListProduct();
+            CargarDatos(lisProductos);
         }
+
+
 
         private void TxtCodigo_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -77,6 +93,46 @@ namespace CapaPresentacion
         private void FrmListaProductos_FormClosed(object sender, FormClosedEventArgs e)
         {
             this.Close();
+        }
+
+        //busqueda por filtro
+        private void BtnFiltrar_Click(object sender, EventArgs e)
+        {
+            lisProductos = NegProd.ListProduct();
+                //buscar
+                IEnumerable<TbProducto> listaAux = new List<TbProducto>();
+
+                if (TxtCodigo.Text != string.Empty)
+                {
+                    listaAux = lisProductos.Where(x => x.CodProducto.ToString().Contains(TxtCodigo.Text)).ToList();
+                }
+                if (TxtNombreProducto.Text != string.Empty)
+                {
+                    listaAux = lisProductos.Where(x => x.NombreProducto.Trim().ToUpper().Contains(TxtNombreProducto.Text.Trim().ToUpper())).ToList();
+                }
+                if (listaAux.Count() == 0 && TxtCodigo.Text != string.Empty && TxtNombreProducto.Text == string.Empty)
+                {
+                    listaAux = lisProductos;
+                }
+            CargarDatos((List<TbProducto>)listaAux);
+            
+        }
+        //Cargamos datos al data grid
+        private void CargarDatos(List<TbProducto> listaProductos)
+        {
+            DgvListaProductos.Rows.Clear();
+     
+
+            foreach (TbProducto tbProducto in listaProductos)
+            {
+                int nr = DgvListaProductos.Rows.Add();
+
+                DgvListaProductos.Rows[nr].Cells[0].Value = tbProducto.CodProducto;
+                DgvListaProductos.Rows[nr].Cells[1].Value = tbProducto.NombreProducto;
+                DgvListaProductos.Rows[nr].Cells[2].Value = tbProducto.CantidadProducto;
+                DgvListaProductos.Rows[nr].Cells[3].Value = tbProducto.CostoProducto;
+                DgvListaProductos.Rows[nr].Cells[4].Value = tbProducto.Descripcion;
+            }
         }
     }
 }
