92fd40bd8efe1bcf9f2eb5ca5da5c4cbc20a9727
KervinRuiz <129538019+KervinRuiz@users.noreply.github.com>
2023-05-01 15:08:07 -0600

    Terminado presupuesto

--- CapaPresentacion/FrmPresupuesto.cs
+++ CapaPresentacion/FrmPresupuesto.cs
@@ -1,4 +1,6 @@
-﻿using Guna.UI2.WinForms;
+﻿using CapaEntidades;
+using CapaNegocios;
+using Guna.UI2.WinForms;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,18 +15,18 @@ namespace CapaPresentacion
 {
     public partial class FrmPresupuesto : Form
     {
-       
-
-
+        NegocioPresupuestos NegocioPresupuestos = new NegocioPresupuestos();
+        TbPresupuesto tbPresupuesto1 = new TbPresupuesto();
         public FrmPresupuesto()
         {
             InitializeComponent();
         }
       
 
-private void BtnVerLista_Click(object sender, EventArgs e)
+        private void BtnVerLista_Click(object sender, EventArgs e)
         {
             FrmListaPresupuestos frmListaPresupuestos = new FrmListaPresupuestos();
+            frmListaPresupuestos.pasarDatosEvent += pasarDatosFormularios;
             frmListaPresupuestos.ShowDialog();
         }
 
@@ -41,12 +43,27 @@ private void BtnVerLista_Click(object sender, EventArgs e)
 
                 if (TxtMontuoProsupuesto.TextLength > 0)
                 {
-                    Validaciones.LimpiarError(TxtMontuoProsupuesto);
+                    //Validaciones.LimpiarError(TxtMontuoProsupuesto);
 
                     //------------------------------------------------------------------------------------------
                     // ACA VA EL CODIGO PARA REALIZAR EL GUARDADO
 
+                    TbPresupuesto tbPresupuesto = new TbPresupuesto();
 
+                    //Llenamos el presupuesto
+                    tbPresupuesto.IdPresupuesto = TxtCodigoPresepuesto.Text;
+                    tbPresupuesto.MesPresupuesto = DtpFechaSolicitud.Value;
+                    tbPresupuesto.MontoPresupuesto = Convert.ToDouble(TxtMontuoProsupuesto.Text);
+                    tbPresupuesto.EstadoPresupuesto = true;
+
+                    if (NegocioPresupuestos.GuardarPresupuesto(tbPresupuesto))
+                    {
+                        MessageBox.Show("Presupuesto agregado","Mensaje",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al Guardar", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
 
                     //------------------------------------------------------------------------------------------
                 }
@@ -74,7 +91,7 @@ private void BtnVerLista_Click(object sender, EventArgs e)
                     //------------------------------------------------------------------------------------------
                     // ACA VA EL CODIGO PARA REALIZAR LA MODIFICACION
 
-
+                    modificar(tbPresupuesto1.EstadoPresupuesto == true);
 
                     //------------------------------------------------------------------------------------------
                 }
@@ -102,8 +119,21 @@ private void BtnVerLista_Click(object sender, EventArgs e)
                     //------------------------------------------------------------------------------------------
                     // ACA VA EL CODIGO PARA REALIZAR LA ELIMINACION
 
-
-
+                    DialogResult dialog = MessageBox.Show("Estas seguro de eliminar este presupuesto", "Alerta", MessageBoxButtons.YesNo,MessageBoxIcon.Exclamation);
+                    if (dialog == DialogResult.Yes)
+                    {
+                        tbPresupuesto1.EstadoPresupuesto = false;
+                        if (NegocioPresupuestos.EliminarPresupuesto(tbPresupuesto1))
+                        {
+                            MessageBox.Show("Presupuesto eliminado", "Alerta",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                            restablecer();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error al eliminar el presupuesto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    restablecer();
                     //------------------------------------------------------------------------------------------
                 }
                 else
@@ -130,6 +160,94 @@ private void BtnVerLista_Click(object sender, EventArgs e)
         private void TxtMontuoProsupuesto_KeyPress(object sender, KeyPressEventArgs e)
         {
             Validaciones.Numeros_Decimales_y_Borrar(e, TxtMontuoProsupuesto);
+        }
+
+        private string ObtenerCodigo()
+        {
+            String Codigo = "";
+
+            NegocioPresupuestos negocio = new NegocioPresupuestos();
+            List<TbPresupuesto> lista = negocio.ListaPresupuestos();
+            TbPresupuesto TbPresupuestos = new TbPresupuesto();
+
+            TbPresupuestos = lista.LastOrDefault();
+
+            if (lista.Count() > 0)
+            {
+                if ((int.Parse(TbPresupuestos.IdPresupuesto) + 1) > 9)
+                {
+                    Codigo = "0000" + (int.Parse(TbPresupuestos.IdPresupuesto) + 1).ToString();
+                    if ((int.Parse(TbPresupuestos.IdPresupuesto) + 1) > 99)
+                    {
+                        Codigo = "000" + (int.Parse(TbPresupuestos.IdPresupuesto) + 1).ToString();
+                    }
+                    else
+                    {
+                        Codigo = "0000" + (int.Parse(TbPresupuestos.IdPresupuesto) + 1).ToString();
+                    }
+                }
+                else
+                {
+                    Codigo = "00000" + (int.Parse(TbPresupuestos.IdPresupuesto) + 1).ToString();
+                }
+            }
+            else
+            {
+                Codigo = "0000";
+            }
+            return Codigo;
+        }
+
+        private void FrmPresupuesto_Load(object sender, EventArgs e)
+        {
+            TxtCodigoPresepuesto.Text = ObtenerCodigo();
+        }
+
+        public void CargarDatosFormulario(TbPresupuesto presupuesto)
+        {
+            TxtCodigoPresepuesto.Text = presupuesto.IdPresupuesto;
+            TxtMontuoProsupuesto.Text = presupuesto.MontoPresupuesto.ToString();
+            DtpFechaSolicitud.Text = presupuesto.MesPresupuesto.ToString();
+        }
+
+        private void pasarDatosFormularios(TbPresupuesto _presupuesto)
+        {
+            tbPresupuesto1 = _presupuesto;
+            CargarDatosFormulario(tbPresupuesto1);
+            BtnModificar.Visible = true;
+            BtnEliminar.Visible = true;
+            BtnGuardarPresupuesto.Visible = false;
+            TxtCodigoPresepuesto.Enabled = false;
+        }
+        private void restablecer()
+        {
+            TxtCodigoPresepuesto.Enabled = true;
+            BtnEliminar.Visible = false;
+            BtnModificar.Visible = false;
+            BtnGuardarPresupuesto.Visible = true;
+        }
+
+        public void modificarDatos(bool estado)
+        {
+            if (estado)
+            {
+                tbPresupuesto1.MontoPresupuesto = double.Parse(TxtMontuoProsupuesto.Text);
+                tbPresupuesto1.MesPresupuesto = DtpFechaSolicitud.Value;
+            }
+            tbPresupuesto1.EstadoPresupuesto = true;
+        }
+        public void modificar(bool estado)
+        {
+            modificarDatos(estado);
+            if(NegocioPresupuestos.EditarPresupuesto(tbPresupuesto1))
+            {
+                MessageBox.Show("Presupuesto modificado","Mensaje",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                restablecer();
+            }
+            else
+            {
+                MessageBox.Show("Error al modificar");
+            }
         }
     }
 }
