d5295123a0aa391f1109430db46076cad2a14245
ALZ-coder <alexlopezzamora17@gmail.com>
2023-05-01 19:00:09 -0600

    ALZ

--- CapaPresentacion/FrmSolicitudInsumos.cs
+++ CapaPresentacion/FrmSolicitudInsumos.cs
@@ -91,6 +91,7 @@ namespace CapaPresentacion
                         else
                         {
                             validaConfirmacion = false;
+                            //hola
 
                         }
                     }
