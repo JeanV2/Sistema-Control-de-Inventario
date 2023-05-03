22c3c031b4365acf9dc1ddc1b739d816229c1b77
FSO Tecnho Systems <fsotecnhosystems@gmail.com>
2023-05-02 19:16:20 -0600

    report

---
+++ CapaPresentacion/FrmHistCompras.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class FrmHistCompras : Form
+    {
+        public FrmHistCompras()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmHistCompras_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
