3736dfc6890f1dd48c515f86cbe56ccf99ee1cc3
JeanV2 <126120857+JeanV2@users.noreply.github.com>
2023-05-08 12:29:46 -0600

    cambios

--- CapaPresentacion/FrmHistCompras.designer.cs
+++ CapaPresentacion/FrmHistCompras.designer.cs
@@ -34,7 +34,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.guna2DateTimePicker2 = new Guna.UI2.WinForms.Guna2DateTimePicker();
             this.BtnFiltrar = new Guna.UI2.WinForms.Guna2Button();
-            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,19 +47,20 @@
             this.flowLayoutPanel1.Controls.Add(this.BtnFiltrar);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1037, 45);
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(67, 0, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1383, 55);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(50, 12);
-            this.label2.Margin = new System.Windows.Forms.Padding(0, 12, 0, 0);
+            this.label2.Location = new System.Drawing.Point(67, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 21);
+            this.label2.Size = new System.Drawing.Size(130, 25);
             this.label2.TabIndex = 14;
             this.label2.Text = "Buscar del ";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -73,12 +73,12 @@
             this.guna2DateTimePicker1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2DateTimePicker1.ForeColor = System.Drawing.Color.Black;
             this.guna2DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.guna2DateTimePicker1.Location = new System.Drawing.Point(154, 3);
-            this.guna2DateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 3, 30, 3);
+            this.guna2DateTimePicker1.Location = new System.Drawing.Point(201, 4);
+            this.guna2DateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 40, 4);
             this.guna2DateTimePicker1.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
             this.guna2DateTimePicker1.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
             this.guna2DateTimePicker1.Name = "guna2DateTimePicker1";
-            this.guna2DateTimePicker1.Size = new System.Drawing.Size(146, 36);
+            this.guna2DateTimePicker1.Size = new System.Drawing.Size(195, 44);
             this.guna2DateTimePicker1.TabIndex = 1;
             this.guna2DateTimePicker1.Value = new System.DateTime(2023, 5, 2, 19, 0, 27, 0);
             // 
@@ -86,10 +86,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(330, 12);
-            this.label1.Margin = new System.Windows.Forms.Padding(0, 12, 20, 0);
+            this.label1.Location = new System.Drawing.Point(436, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(0, 15, 27, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 21);
+            this.label1.Size = new System.Drawing.Size(98, 25);
             this.label1.TabIndex = 15;
             this.label1.Text = "hasta el";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -102,12 +102,12 @@
             this.guna2DateTimePicker2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2DateTimePicker2.ForeColor = System.Drawing.Color.Black;
             this.guna2DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.guna2DateTimePicker2.Location = new System.Drawing.Point(430, 3);
-            this.guna2DateTimePicker2.Margin = new System.Windows.Forms.Padding(3, 3, 30, 3);
+            this.guna2DateTimePicker2.Location = new System.Drawing.Point(565, 4);
+            this.guna2DateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 40, 4);
             this.guna2DateTimePicker2.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
             this.guna2DateTimePicker2.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
             this.guna2DateTimePicker2.Name = "guna2DateTimePicker2";
-            this.guna2DateTimePicker2.Size = new System.Drawing.Size(146, 36);
+            this.guna2DateTimePicker2.Size = new System.Drawing.Size(195, 44);
             this.guna2DateTimePicker2.TabIndex = 16;
             this.guna2DateTimePicker2.Value = new System.DateTime(2023, 5, 2, 19, 0, 27, 0);
             // 
@@ -131,33 +131,24 @@
             this.BtnFiltrar.HoverState.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnFiltrar.Image = global::CapaPresentacion.Properties.Resources.filter_52px;
             this.BtnFiltrar.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.BtnFiltrar.Location = new System.Drawing.Point(609, 7);
-            this.BtnFiltrar.Margin = new System.Windows.Forms.Padding(3, 7, 3, 30);
+            this.BtnFiltrar.Location = new System.Drawing.Point(804, 9);
+            this.BtnFiltrar.Margin = new System.Windows.Forms.Padding(4, 9, 4, 37);
             this.BtnFiltrar.Name = "BtnFiltrar";
             this.BtnFiltrar.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(208)))), ((int)(((byte)(225)))));
-            this.BtnFiltrar.Size = new System.Drawing.Size(95, 32);
+            this.BtnFiltrar.Size = new System.Drawing.Size(107, 37);
             this.BtnFiltrar.TabIndex = 17;
             this.BtnFiltrar.Text = "Filtrar ";
             this.BtnFiltrar.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.BtnFiltrar.UseTransparentBackground = true;
             // 
-            // reportViewer1
-            // 
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.reportViewer1.Location = new System.Drawing.Point(0, 45);
-            this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(1037, 511);
-            this.reportViewer1.TabIndex = 1;
-            // 
             // FrmHistCompras
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1037, 556);
-            this.Controls.Add(this.reportViewer1);
+            this.ClientSize = new System.Drawing.Size(1383, 684);
             this.Controls.Add(this.flowLayoutPanel1);
             this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmHistCompras";
             this.Text = "FrmHistCompras";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmHistCompras_FormClosed);
@@ -176,6 +167,5 @@
         private System.Windows.Forms.Label label1;
         private Guna.UI2.WinForms.Guna2DateTimePicker guna2DateTimePicker2;
         private Guna.UI2.WinForms.Guna2Button BtnFiltrar;
-        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
     }
 }
